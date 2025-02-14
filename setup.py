bbdfd3d9412bf0fd669235e9e07356d9fbd05e10
Konig Janos <janos.koenig@gmail.com>
2025-02-14 07:10:50 +0100

    digitalocean deploy

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="your_app_name",
+    version="1.0.0",
+    install_requires=[
+        "Flask==2.2.3",
+        "Werkzeug==2.2.3",
+        "mysql-connector-python==8.0.33",
+        "python-dotenv==1.0.0",
+        "Pillow==10.2.0"
+    ]
+)
